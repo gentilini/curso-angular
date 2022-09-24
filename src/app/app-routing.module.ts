1a50f22edc8a37aa4d6f8654229827b81c21751f
Rafael Gentilini <gentilini.ads@gmail.com>
2022-09-23 23:11:45 -0300

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponentComponent } from './components/list-render-component/list-render-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: FirstComponentComponent},
+  {path:'list', component: ListRenderComponentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
